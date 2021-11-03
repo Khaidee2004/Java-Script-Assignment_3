c10fba3f4866e52f58487e8dfc90846c82aed37f
Khaidee2004 <khalil.haider2109@gmail.com>
2021-11-03 16:13:05 -0700

    assignment 3 completed

---
+++ Arrays/app.js
@@ -0,0 +1,59 @@
+// question .1 
+
+
+
+// question .2 
+
+
+
+// question .3 
+
+
+
+// question .4 
+
+
+
+// question .5 
+
+
+
+// question .6 
+
+
+
+// question .7 
+
+
+
+// question .8 
+
+
+
+// question .9 
+
+
+
+// question .10
+
+
+
+// question .11
+
+
+
+// question .12
+
+
+
+// question .13
+
+
+
+// question .14
+
+
+
+// question .15
+
+
